71dd6bfaf4e22f0f9468225899a3936c87597be8
Natsune <me@natsuneko.cat>
2022-01-30 20:42:31 +0900

    feat: generate RSS feed

---
+++ pages/feed.tsx
@@ -0,0 +1,63 @@
+import { writeFile } from "fs/promises";
+import { GetStaticProps } from "next";
+import { Feed as RssFeed } from "feed";
+import marked from "marked";
+
+import { getEntries } from "../utils/fs";
+
+const generateRssFeed = async (): Promise<void> => {
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || "";
+  const date = new Date();
+  const author = {
+    name: "Natsuneko",
+    email: "me@natsuneko.cat",
+    link: "https://natsuneko.cat",
+  };
+
+  const feed = new RssFeed({
+    title: "なつねこメモ",
+    description: "主にプログラミング関連のメモ帳 ♪(✿╹ヮ╹)ﾉ",
+    id: baseUrl,
+    link: baseUrl,
+    language: "ja",
+    image: `${baseUrl}/favicon.ico`,
+    author,
+    copyright: `All Rights Reserved 2015 - ${date.getFullYear()} ${
+      author.name
+    }`,
+    updated: date,
+    feedLinks: {
+      rss2: `${baseUrl}/rss/feed.xml`,
+      json: `${baseUrl}/rss/feed.json`,
+      atom: `${baseUrl}/rss/atom.xml`,
+    },
+  });
+
+  const entries = await getEntries();
+  for (const entry of entries) {
+    feed.addItem({
+      title: entry.title,
+      description: "no description provided",
+      id: `${baseUrl}/entry/${entry.basename}`,
+      link: `${baseUrl}/entry/${entry.basename}`,
+      content: marked(entry.content),
+      date: new Date(entry.date),
+    });
+  }
+
+  await writeFile("./public/rss/feed.xml", feed.rss2());
+  await writeFile("./public/rss/feed.json", feed.json1());
+  await writeFile("./public/rss/atom.xml", feed.atom1());
+};
+
+const getStaticProps: GetStaticProps = async () => {
+  await generateRssFeed();
+
+  return { props: {} };
+};
+
+const Feed: React.VFC = () => {
+  return <></>;
+};
+
+export default Feed;
