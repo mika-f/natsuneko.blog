17a213936fecb25246d46599bb92392fb02b4158
Natsune <me@natsuneko.cat>
2022-02-07 13:52:43 +0900

    Revert "fix: fix style for image"
    This reverts commit 6ed5427537917902d69158cd3a0d69838eb99f75.

--- components/ZoomableImage.tsx
+++ components/ZoomableImage.tsx
@@ -17,7 +17,7 @@ const ZoomableImage: React.VFC<Props> = ({ src, alt }) => {
 
   return (
     <Zoom>
-      <div className="w-full block my-4">
+      <div className="w-full block my-4 text-center">
         <Image
           src={source}
           alt={alt ?? "image"}
